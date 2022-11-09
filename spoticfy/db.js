855d10241df1c7403cbfe61b44dc2ffb05c3981a
HolaMundo42 <77760629+HolaMundo42@users.noreply.github.com>
2022-11-09 08:49:03 -0300

    setup

--- spoticfy/db.js
+++ spoticfy/db.js
@@ -2,6 +2,19 @@ const mysql = require("mysql2");
 
 const conn = mysql.createConnection({
     // Completar con los datos de la conexión
+    host: "localhost",
+    user: "root",
+    password: "rootroot",
+    database: "spoticfy",
+});
+
+conn.connect((err) => {
+    if (err) {
+        console.error("Error conectándose: " + err);
+        return;
+    }
+
+    console.log("Base de datos conectada");
 });
 
 module.exports = conn;
