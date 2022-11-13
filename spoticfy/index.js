4bc6c389832e02d37c40f40962b4162d6d8af0a0
HolaMundo42 <pesimaconexion@gmail.com>
2022-11-13 00:19:03 -0300

    port 3000

--- spoticfy/index.js
+++ spoticfy/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3002;
+const port = 3000;
 
 const artistas = require("./controllers/artistas");
 const albumes = require("./controllers/albumes");
